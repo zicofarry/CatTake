3ed91ef677e9c906e06c7870d4d210d07757c7dd
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-11-25 11:12:20 +0700

    add configuration for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,25 @@
 // Contoh koneksi sederhana menggunakan 'pg' (PostgreSQL)
 const { Pool } = require('pg');
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'cattake',
-    password: '3636',
-    port: 5432,
-});
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionConfig = isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false // Diperlukan untuk koneksi SSL Railway
+        }
+      }
+    : {
+        user: process.env.DB_USER || 'postgres',
+        host: process.env.DB_HOST || 'localhost',
+        database: process.env.DB_NAME || 'cattake',
+        password: process.env.DB_PASS || '3636',
+        port: process.env.DB_PORT || 5432,
+    };
+    
+const pool = new Pool(connectionConfig);
+
 
 const connectDB = async () => {
     try {
